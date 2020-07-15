ac8394e4c38e960a45f37801d78ac07ed733146f
comnic <comnics@gmail.com>
2020-07-15 09:29:41 +0900

    lec8

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost/test',{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    console.log(req.body)
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
