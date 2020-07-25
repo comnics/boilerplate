8d54d6c87ede1d558dc605f485e5e17925f4559b
comnic <comnics@gmail.com>
2020-07-25 09:52:58 +0900

    client init

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,25 @@ import * as serviceWorker from './serviceWorker';
 
 import 'antd/dist/antd.css';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENTION__()
+      )}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
