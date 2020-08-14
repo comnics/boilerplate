379cb3dcaf48adcc1679ed2ab5f05e9f19adb6ec
comnic <comnics@gmail.com>
2020-08-14 10:18:14 +0900

    Resolve IE9,10,11 issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,10 @@
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
